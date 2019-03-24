3385f71bab345d671ae9b077d5e6251bf48a0be1
frankarinze <arinze086@gmail.com>
2019-03-24 18:52:55 +0100

    Add files via upload

---
+++ frank/js/custom.js
@@ -0,0 +1,93 @@
+/*-----------------------------------------------------------
+* Template Name    : Frank | Portfolio Template
+* Author           : Frank Arinze
+* Version          : 1.0.0
+* Created          : March 2019
+* Descripting      : js main
+*------------------------------------------------------------
+*/
+
+$('#status').fadeOut();
+$('#preloader').delay(350).fadeOut('slow');
+$('body').delay(350).css({
+    'overflow': ' '
+});
+
+$(window).on('scroll',function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+        $(".sticky").addClass("stickyadd");
+    } else {
+        $(".sticky").removeClass("stickyadd");
+    }
+});
+
+$('.navbar-nav a').on('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 0
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+$("#navbarCollapse").scrollspy({
+    offset:20
+});
+
+$(".element").each(function() {
+    var $this = $(this);
+    $this.typed({
+        strings: $this.attr('data-elements').split(','),
+        typeSpeed: 100,
+        backDelay: 3000
+    });
+});
+$(window).on('load', function () {
+    var $container = $('.work-filter');
+    var $filter = $('#menu-filter');
+    $container.isotope({
+        filter: '*',
+        layoutMode: 'masonry',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear'
+        }
+    });
+
+    $filter.find('a').on("click",function() {
+        var selector = $(this).attr('data-filter');
+        $filter.find('a').removeClass('active');
+        $(this).addClass('active');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                animationDuration: 750,
+                easing: 'linear',
+                queue: false,
+            }
+        });
+        return false;
+    });
+});
+
+$('.img-zoom').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-fade',
+    gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+    }
+});
+
+$("#owl-demo").owlCarousel({
+    autoPlay: 7000,
+    stopOnHover: true,
+    navigation: false,
+    paginationSpeed: 1000,
+    goToFirstSpeed: 2000,
+    singleItem: true,
+    autoHeight: true,
+});
